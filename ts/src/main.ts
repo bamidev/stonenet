a615d6dca98a9ecd4de101760017c94a2593e80e
bamidev <bamidev@protonmail.com>
2024-02-15 00:00:28 +0100

    Improve the milkdown stuff.

--- ts/src/main.ts
+++ ts/src/main.ts
@@ -1,8 +1,9 @@
-import { defaultValueCtx, Editor, rootCtx } from '@milkdown/core';
+import { defaultValueCtx, editorViewOptionsCtx, Editor, rootCtx } from '@milkdown/core';
 import { commonmark } from '@milkdown/preset-commonmark';
 import { block } from '@milkdown/plugin-block';
 import { clipboard } from '@milkdown/plugin-clipboard';
 import { indent, indentConfig } from '@milkdown/plugin-indent';
+import { listener, listenerCtx } from '@milkdown/plugin-listener';
 import { math } from '@milkdown/plugin-math';
 import { prism, prismConfig } from '@milkdown/plugin-prism';
 
@@ -15,39 +16,91 @@ import rust from 'refractor/lang/rust'
 import tsx from 'refractor/lang/tsx'
 import typescript from 'refractor/lang/typescript'
 
+import './main.css'
 
-// Hide the actual form element
-let elements = document.getElementsByClassName("no-editor")
-for (let i in elements) {
-	let e = elements[i]
-	console.log(elements)
-	if (e instanceof Element) { e.setAttribute("style", "display: none;") }
+
+function htmlToText(html: string): string {
+	return html
+		.replace(/<br>/g, "\n")
+		.replace(/&gt;/g, ">")
+		.replace(/&lt;/g, "<")
+		.replace(/&amp;/g, "&")
 }
 
-Editor
-	.make()
-	.config(ctx => {
-		ctx.set(indentConfig.key, {
-			type: 'tab',
-			size: 4,
+function load_markdown(selector: string, editable: boolean, content?: string) {
+
+	if (editable) {
+		var underlying_element = <HTMLTextAreaElement>document.querySelector(selector + ' + textarea.default-editor')
+		underlying_element.setAttribute("style", "display: none;")
+	}
+
+	Editor
+		.make()
+		.config(ctx => {
+			if (content) { console.log('content'); console.log(content)
+				ctx.set(defaultValueCtx, content)
+			}
+			ctx.set(editorViewOptionsCtx, { editable: () => editable })
+			ctx.set(indentConfig.key, {
+				type: 'tab',
+				size: 4,
+			})
+			ctx.set(prismConfig.key, {
+				configureRefractor: (refractor) => {
+					refractor.register(css_lang)
+					refractor.register(javascript)
+					refractor.register(jsx)
+					refractor.register(markdown)
+					refractor.register(rust)
+					refractor.register(tsx)
+					refractor.register(typescript)
+				},
+			})
+			ctx.set(rootCtx, selector)
+
+			if (editable) {
+				const listener = ctx.get(listenerCtx)
+				listener.markdownUpdated((ctx, markdown, prev_markdown) => {
+					if (markdown !== prev_markdown) {
+						// It seems that a bug in markown places an HTML escaped
+						// space character at the end sometimes instead of a
+						// normal space
+						if (markdown.endsWith('&#x20;\n')) {
+							markdown = markdown.substring(
+								0, markdown.length - 7
+							) + ' \n'
+						}
+						underlying_element.value = markdown
+					}
+				})
+			}
 		})
-		ctx.set(prismConfig.key, {
-			configureRefractor: (refractor) => {
-				refractor.register(css_lang)
-				refractor.register(javascript)
-				refractor.register(jsx)
-				refractor.register(markdown)
-				refractor.register(rust)
-				refractor.register(tsx)
-				refractor.register(typescript)
-			  },
-		})
-		ctx.set(rootCtx, '.editor')
-	})
-	.use(block)
-	.use(commonmark)
-	.use(clipboard)
-	.use(indent)
-	.use(math)
-	.use(prism)
-	.create();
+		.use(block)
+		.use(commonmark)
+		.use(clipboard)
+		.use(indent)
+		.use(listener)
+		.use(math)
+		.use(prism)
+		.create();
+}
+
+function load_markdown_content() {
+	let elements = document.getElementsByClassName("content-markdown")
+	for (let i in elements) {
+		let element = elements[i]
+		if (element instanceof HTMLElement ) {
+			let el = <HTMLElement>element;
+			el.setAttribute("style", "display: none; white-space: pre-wrap;")
+			let content = htmlToText(el.innerHTML.trimStart().trimEnd());
+
+			let id = element.id.substring("milkdown-content-".length)
+			console.log("ID = " + id)
+			load_markdown('#milkdown-view-' + id, false, content)
+		}
+	}
+}
+
+
+load_markdown("#editor", true, undefined)
+load_markdown_content()
