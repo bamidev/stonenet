3f353415b2ba03c9b690f2809bf2fc5a9e64f626
Bamidev <bamidev@pm.me>
2024-03-14 12:34:19 +0100

    Use the next browser-window api for stonenet-desktop.

--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -6,13 +6,13 @@ fn main() {
 	let settings = ApplicationSettings::default();
 	let application = Application::initialize(&settings).unwrap();
 	let runtime = application.start();
-	let exit_code = runtime.run_async(|handle| async {
+	let exit_code = runtime.run_async(|handle| async move {
 		let source = Source::Url("http://localhost:37338".into());
 		let mut bwb = BrowserWindowBuilder::new(source);
 		bwb.dev_tools(true);
 		bwb.title("Stonenet");
 		bwb.size(1024, 768);
-		let bw = bwb.build(handle).await;
+		let bw = bwb.build_async(&handle).await;
 		bw.show();
 	});
 
