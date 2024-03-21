03423ddfa1d1242a901ccf7bef1a93171f4539d2
bamidev <bamidev@protonmail.com>
2024-03-21 09:18:11 +0100

    Build & install stonenet-desktop too from the install.sh script.

--- install.sh
+++ install.sh
@@ -10,6 +10,8 @@ EXECUTABLE_PATH="$PREFIX/bin/stonenetd"
 CONFIG_PATH="$PREFIX/etc/stonenet"
 DATA_FILES_PATH="$PREFIX/share/stonenet"
 LOGFILE="$PREFIX/var/log/stonenet.log"
+# Build & install the desktop app as well.
+#USE_DESKTOP_APP=1
 # If your system is using Systemd, just uncomment the following line to enable
 # the Systemd service:
 #SYSTEMD_PATH=/lib/systemd/system
@@ -23,7 +25,13 @@ LOGFILE="$PREFIX/var/log/stonenet.log"
 
 set -e
 cargo build --release
+if -n "$USE_DESKTOP_APP"; then
+    cargo build -p stonenet-desktop --release
+fi
 install target/release/stonenetd "$EXECUTABLE_PATH"
+if -n "$USE_DESKTOP_APP"; then
+    install target/release/stonenet-desktop "$EXECUTABLE_PATH"
+fi
 install conf/base.toml "$CONFIG_PATH/config.toml"
 install -t static "$DATA_FILES_PATH"
 install -t templates "$DATA_FILES_PATH"
