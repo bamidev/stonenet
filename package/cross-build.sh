b28ac53e87c61b2dd6c75487adf722e26bc72132
Bamidev <bamidev@pm.me>
2025-11-13 20:35:31 +0100

    Improve the cross-compilation scripts

--- package/cross-build.sh
+++ package/cross-build.sh
@@ -2,15 +2,19 @@
 # Build all the debian packages and the windows installer using docker
 set -ex
 
+
 function build() {
-	docker build -t $1 . --build-arg target=$1
-	CONTAINER_ID=$(docker create $1)
-	rm -rf ./$1
-	docker cp $CONTAINER_ID:/home/stonenet/out ./$1
+	docker build -t $2 . --build-arg target=$2
+	CONTAINER_ID=$(docker create $2)
+	rm -rf ./$1/$2
+	docker cp $CONTAINER_ID:/home/stonenet/out ./out/$1/$2
 	docker rm $CONTAINER_ID
 }
 
-build amd64
-build arm64
-build armhf
-build win64
+
+rm -rf ./out
+mkdir -p ./out/{debian,windows}
+build debian amd64
+build debian arm64
+build debian armhf
+build windows win64
